5cb2ba4b70ecb1174144394e66bf6a64ffb89d77
kushvarma-solute <125448925+kushvarma-solute@users.noreply.github.com>
2023-04-11 15:20:27 +0530

    Add files via upload

---
+++ RestApi/Backend/test/auth-middleware.js
@@ -0,0 +1,53 @@
+const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon')
+const authMiddleware = require('../middleware/is-auth');
+
+describe("Auth middleware",function(){
+    it('Should throw an error if no authorization header is present',function(){
+        const req = {
+            get: function(headerName){
+                return null;
+            }
+        };
+        expect(authMiddleware.bind(this,req,{},() => {})).to.throw(
+            'Not authenticated'
+        );
+    })
+    
+    it('Should throw an error if the authorization header is only one string',function(){
+        const req = {
+            get: function(headerName){
+                return "xyz";
+            }
+        };
+        expect(authMiddleware.bind(this,req,{},() => {})).to.throw();
+    })
+
+    it('Should yield a userId after decoding the token',function(){
+        const req = {
+            get: function(headerName){
+                return "Bearer agscusacuhuasihauas";
+            }
+        };
+        sinon.stub(jwt,'verify');
+      jwt.verify.returns({userId:'abc'});
+        authMiddleware(req,{},() => {})
+        expect(req).to.have.property('userId');
+        expect(req).to.have.property('userId','abc');
+        expect(jwt.verify.called).to.be.true;
+        jwt.verify.restore();
+    })
+
+    
+    it('Should throw an error if the token connot be verified',function(){
+        const req = {
+            get: function(headerName){
+                return "Bearer xyz";
+            }
+        };
+        expect(authMiddleware.bind(this,req,{},() => {})).to.throw();
+    })
+
+})
+
---
+++ RestApi/Backend/test/start.js
@@ -0,0 +1,12 @@
+// const expect = require('chai').expect;
+// it('should add number correctly',function(){
+//     const num1 = 2;
+//     const num2 = 3;
+//     expect(num1 + num2).to.equal(5);
+// })
+
+// it('should not give result of 6',function(){
+//     const num1 = 2;
+//     const num2 = 3;
+//     expect(num1 + num2).not.to.equal(6);
+// })
